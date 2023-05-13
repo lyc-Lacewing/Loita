e47d06deaf2063eba2ecbb69de497c4a6c95afb1
lyc-Lacewing <lycrlsu01@gmail.com>
2023-05-14 00:23:06 +0800

    namings

--- Loita.cs
+++ Loita.cs
@@ -7,18 +7,22 @@ namespace Loita
         /*
 		 * 法杖: Wand, 施法媒介
 		 * 槽位: Slot, 法杖的容量
-		 * 槽位的填充物: Infusion, 包含各类法术与修饰
+		 * 灌注: Infusion, 包含各类法术与修饰
 		 * 法术: Spell, 包含各类法术
 		 * 动态法术: Dynamic Spell, 施放与生效快速且动态的法术, 如火花魔弹
 		 * 静态法术: Static Spell, 静态且生效较慢的法术, 如场地魔法
 		 * 被动法术: Passive Spell, 无需施法, 自动施放的法术
 		 * 法术修饰: Modifier, 法术的修饰, 改变法术的属性
+		 * 多放灌注: Confluxer, 用于多重施放的灌注
 		 * 施法: Cast, 施放法术的动作
 		 * 序列: Sequence, 施法前, 法术与修饰会按顺序排好, 依次施放
 		 * 延迟: Delay, 两个法术间的延迟
 		 * 充能: Recharge, 每轮施法后, 法杖需要一定时间充能, 才可开始下一轮施法
-		 * 魔耗: Mana Cost, 施放法术需要消耗的魔力
 		 * 随机偏移: Scatter, 法术在施放时随机偏移
+		 * 法术级联: Cascade, 一个法术触发其它法术的现象
+		 * 触发法术: Initiator, 法术级联中的上游法术
+		 * 跟随法术: Follower, 法术级联中的下游法术
+		 * 多重施放: Conflux, 多个法术受同一组修饰影响并同时施放
 		 */
     }
 }
